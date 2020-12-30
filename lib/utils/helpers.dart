337d39dd5a6903c2309b1c74015db9513c6b9ab0
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-30 17:23:01 +0800

    Rename to vw & vh

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-double responsiveWidth(BuildContext context, double percentage) {
+double viewWidth(BuildContext context, double percentage) {
   return MediaQuery.of(context).size.width * (percentage / 100);
 }
 
-double responsiveHeight(BuildContext context, double percentage) {
+double viewHeight(BuildContext context, double percentage) {
   return MediaQuery.of(context).size.height * (percentage / 100);
 }
