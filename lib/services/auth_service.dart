2d134c91c29be5268a5523ac545ba7beeeff8936
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-31 07:58:01 +0800

    Add AuthService

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential> login({String email, String password}) async {
+    UserCredential userCredential;
+    try {
+      userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-email') {
+        throw 'The email is invalid.';
+      } else if (e.code == 'user-not-found') {
+        throw 'No account created for this email.';
+      } else if (e.code == 'wrong-password') {
+        throw 'The password is incorrect. ';
+      } else {
+        throw e.message;
+      }
+    }
+    return userCredential;
+  }
+
+  Future<UserCredential> signUp({String email, String password}) async {
+    UserCredential userCredential;
+    try {
+      userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-email') {
+        throw 'The email is invalid.';
+      } else if (e.code == 'weak-password') {
+        throw 'The password is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        throw 'An account already exists for this email.';
+      } else {
+        throw e.message;
+      }
+    }
+    return userCredential;
+  }
+}
