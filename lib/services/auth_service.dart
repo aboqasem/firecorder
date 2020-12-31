c1259fc2bdbba3fd4ee47fac3bd75254c9882f9d
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-31 09:10:07 +0800

    Add get userStream

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Stream<User> get userStream => _auth.authStateChanges();
+
   Future<UserCredential> login({String email, String password}) async {
     UserCredential userCredential;
     try {
