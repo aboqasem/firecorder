28666b7e0ddb59196f863d772209cd7916494837
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-31 10:18:18 +0800

    Initialize FireFlutter

--- lib/screens/launch_screen.dart
+++ lib/screens/launch_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firecorder/utils/helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LaunchScreen extends StatefulWidget {
   @override
@@ -6,26 +9,61 @@ class LaunchScreen extends StatefulWidget {
 }
 
 class _LaunchScreenState extends State<LaunchScreen> {
+  bool _isLoading = true, _isError = false;
+
+  void initializeFlutterFire() async {
+    try {
+      await Firebase.initializeApp();
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _isError = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextButton(
-              child: Text('Home'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
+            Icon(
+              Icons.graphic_eq,
+              size: viewWidth(context, 50),
             ),
-            TextButton(
-              child: Text('Authentication'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/auth');
-              },
+            SizedBox(
+              height: viewHeight(context, 10),
             ),
+            _isLoading
+                ? SpinKitCircle(
+                    color: Colors.black,
+                    size: viewHeight(context, 5),
+                  )
+                : _isError
+                    ? Text(
+                        'An error occurred while initializing the app.',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: viewHeight(context, 2),
+                        ),
+                      )
+                    : Text(
+                        'Initialized.',
+                        style: TextStyle(
+                          fontSize: viewHeight(context, 2),
+                        ),
+                      ),
           ],
         ),
       ),
