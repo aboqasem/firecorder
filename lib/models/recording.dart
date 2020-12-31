efeb0d3c67e7c18349df2ec5b8eb3350646c0a26
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-31 20:57:43 +0800

    Encapsulate Recording's data

--- lib/models/recording.dart
+++ lib/models/recording.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Recording {
-  String name;
-  bool isPlaying;
+  String _name;
+  bool _isPlaying;
 
-  Recording({@required this.name, this.isPlaying = false});
+  Recording({@required String name, bool isPlaying = false})
+      : _name = name,
+        _isPlaying = isPlaying;
+
+  String get name => _name;
+
+  bool get isPlaying => _isPlaying;
 
   void toggleState() {
-    isPlaying = !isPlaying;
+    _isPlaying = !_isPlaying;
   }
 }
