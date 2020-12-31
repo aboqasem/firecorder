e1b872c78dfd8285c109fba958906b51361fcf51
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-31 16:35:06 +0800

    Implement logout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firecorder/services/auth_service.dart';
 import 'package:firecorder/utils/helpers.dart';
 import 'package:flutter/material.dart';
 
@@ -7,11 +8,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Recordings'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            tooltip: 'Logout',
+            onPressed: () async {
+              await _auth.logout();
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: viewWidth(context, 5)),
