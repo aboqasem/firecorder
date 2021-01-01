b587cb6d08e053b8e9b133a82a2d0e791fefd5a1
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-01 20:36:02 +0800

    Upload links to firestore

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firecorder/services/auth_service.dart';
 
 class StorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final AuthService _auth = AuthService();
 
   Future<List<String>> get userRecordings async {
@@ -20,8 +22,15 @@ class StorageService {
   Future<void> uploadRecording({String name, File file}) async {
     final String uid = (await _auth.userStream.first).uid;
     try {
-      await _storage.ref('users/$uid/$name').putFile(file);
-    } on FirebaseException catch (e) {
+      TaskSnapshot task = await _storage.ref('users/$uid/$name').putFile(file);
+      String recordingLink = await task.ref.getDownloadURL();
+      CollectionReference recordings = _firestore.collection('recordings');
+      await recordings.add({
+        'uid': uid,
+        'recording_name': name,
+        'recording_link': recordingLink,
+      });
+    } catch (e) {
       print(e.toString());
     }
   }
