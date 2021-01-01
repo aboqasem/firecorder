7ed1579c0635a326da83e283f65ace0195de91ee
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-01 21:34:30 +0800

    Lift LoadingSpinner & ErrorText to widgets

--- lib/screens/launch_screen.dart
+++ lib/screens/launch_screen.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firecorder/screens/launch_screen/error_text.dart';
+import 'package:firecorder/screens/widgets/error_text.dart';
 import 'package:firecorder/screens/launch_screen/launch.dart';
-import 'package:firecorder/screens/launch_screen/loading_spinner.dart';
+import 'package:firecorder/screens/widgets/loading_spinner.dart';
 import 'package:firecorder/screens/widgets/logo.dart';
 import 'package:firecorder/services/auth_service.dart';
 import 'package:firecorder/utils/helpers.dart';
@@ -54,7 +54,7 @@ class _LaunchScreenState extends State<LaunchScreen> {
             _isLoading
                 ? LoadingSpinner()
                 : _isError
-                    ? ErrorText()
+                    ? ErrorText('An error occurred while initializing the app.')
                     : null,
           ],
         ),
--- lib/screens/widgets/error_text.dart
+++ lib/screens/widgets/error_text.dart
@@ -2,10 +2,14 @@ import 'package:firecorder/utils/helpers.dart';
 import 'package:flutter/material.dart';
 
 class ErrorText extends StatelessWidget {
+  final String text;
+
+  ErrorText(this.text);
+
   @override
   Widget build(BuildContext context) {
     return Text(
-      'An error occurred while initializing the app.',
+      text,
       style: TextStyle(
         color: Colors.red,
         fontSize: viewHeight(context, 2),
