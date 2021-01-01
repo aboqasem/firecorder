51ca823b5bb168d98772601545f67793524b687f
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-01 20:13:19 +0800

    Refactor Recordings model and service

--- lib/screens/home_screen/recordings_list_view.dart
+++ lib/screens/home_screen/recordings_list_view.dart
@@ -1,24 +1,24 @@
 import 'package:firecorder/models/recording.dart';
-import 'package:firecorder/models/recordings_database.dart';
 import 'package:firecorder/screens/home_screen/recording_tile.dart';
+import 'package:firecorder/services/recordings_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class RecordingsListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<RecordingsDatabase>(
-      builder: (BuildContext context, RecordingsDatabase recordingsDatabase,
+    return Consumer<RecordingsService>(
+      builder: (BuildContext context, RecordingsService recordingsService,
           Widget child) {
         return ListView.builder(
-          itemCount: recordingsDatabase.recordings.length,
+          itemCount: recordingsService.count,
           itemBuilder: (BuildContext context, int index) {
-            final Recording recording = recordingsDatabase.recordings[index];
+            final Recording recording = recordingsService.asList[index];
             return RecordingTile(
               name: recording.name,
               isPlaying: recording.isPlaying,
               onTap: () {
-                recordingsDatabase.toggleStateOf(recording);
+                recordingsService.toggleStateOf(recording.name);
               },
             );
           },
